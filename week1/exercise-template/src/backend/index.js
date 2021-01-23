ee3c4ea638e0676b6319e09b34536df023b8cc26
Nathan Kuik <nathan@nathankuik.com>
2021-01-23 15:03:58 +0100

    Initialize improve-week-1-exercise-description

---
+++ week1/exercise-template/src/backend/index.js
@@ -0,0 +1,29 @@
+// explain express comes from node_modules that comes from writing npm install express -s
+// Take a look inside the folder!
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send(`
+    <body>
+        <h1>My portfolio</h1>
+    </body>
+  `);
+});
+
+app.get("/contact", (request, response) => {
+  response.send(`
+    <body>
+        <h1>Contact</h1>
+        <p>This is a contact page</p>
+    </body>
+  `);
+});
+
+
+const server = app.listen(3000, function () {
+  console.log(`> Ready on http://localhost:3000`);
+});
+
+// Export app for testing purposes
+module.exports = server;
